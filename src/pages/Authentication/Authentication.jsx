b29e402ba2ec61f126dd6d47deee0bc6e68168d3
Mateusz Izdebski <mati2k51@gmail.com>
2023-12-07 18:36:52 +0100

    Init projekt, strony logowania oraz rejestracji

---
+++ src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,37 @@
+import { Card, Grid } from '@mui/material';
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+
+const Authentication = () => {
+  return (
+    <div>
+      <Grid container>
+        <Grid className="h-screen overflow-hidden" item xs={7}>
+          <img
+            className="h-full w-full"
+            src="https://www.hubspot.com/hubfs/114_Twitter-Marketing.png"
+            alt=""
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <div className="px-20 flex-col justify-center h-full flex items-center">
+            <Card className="card p-8">
+              <div className="flex flex-col items-center mb-5 space-y-1">
+                <h1 className="log text-center">Twitter</h1>
+                <p className="text-center text-sm w-[70@]">
+                  Connecting Lives, Sharing Stories: Your Social World, Your Way
+                </p>
+              </div>
+
+              {/* <Login /> */}
+              <Register />
+            </Card>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Authentication;
---
+++ src/pages/Authentication/Register.jsx
@@ -0,0 +1,146 @@
+import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import * as Yup from 'yup';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  gender: '',
+};
+const validationSchema = {
+  eamil: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is Required'),
+};
+const Register = () => {
+  const [formValue, setFormValue] = useState();
+
+  const handleSubmit = (values) => {
+    values.gender = gender
+    console.log('handle submit', values);
+  };
+
+  const [gender, setGender] = useState('');
+
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
+
+  return (
+    <>
+      <Formik
+        onSubmit={handleSubmit}
+        // validationSchema={validationSchema}
+        initialValues={initialValues}
+      >
+        <Form className="space-y-5">
+          <div className="space-y-5">
+            <div>
+              <Field
+                as={TextField}
+                name="firstName"
+                placeholder="FirstName"
+                type="text"
+                variant="outlined"
+                fullWidth
+              />
+              <ErrorMessage
+                name="firstName"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div>
+              <Field
+                as={TextField}
+                name="lastName"
+                placeholder="LastName"
+                type="text"
+                variant="outlined"
+                fullWidth
+              />
+              <ErrorMessage
+                name="lastName"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div>
+              <Field
+                as={TextField}
+                name="email"
+                placeholder="Email"
+                type="email"
+                variant="outlined"
+                fullWidth
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div>
+              <Field
+                as={TextField}
+                name="password"
+                placeholder="Password"
+                type="password"
+                variant="outlined"
+                fullWidth
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div>
+              <RadioGroup
+                onChange={handleChange}
+                row
+                aria-label="gender"
+                name="gender"
+              >
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <ErrorMessage
+                  name="gender"
+                  component="div"
+                  className="text-red-500"
+                />
+              </RadioGroup>
+            </div>
+          </div>
+          <Button
+            sx={{ padding: '.8rem 0rem' }}
+            fullWidth
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Register
+          </Button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+export default Register;
